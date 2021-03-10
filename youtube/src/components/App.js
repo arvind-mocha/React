2add72d91a0a394b15ade5d3180b896bd80b0b8f
arvind l <arvindarvind2210@gmail.com>
2021-03-10 14:16:58 +0530

    Initial commit

--- youtube/src/components/App.js
+++ youtube/src/components/App.js
@@ -1,22 +1,32 @@
-import React from "react";
-import SearchBar from "./SearchBar";
-import youtube from "../api/youtube";
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+
+const KEY = 'AIzaSyCN6e98GjAfa_d4E4mISSn8uCP6g5b1DvE';
 
 class App extends React.Component {
-  onSearch = async (search) => {
-    const response = await youtube.get("/search", {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
-        q: search,
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: KEY,
       },
     });
 
-    console.log(response.data);
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onSearch={this.onSearch} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
--- youtube/src/components/SearchBar.js
+++ youtube/src/components/SearchBar.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React from 'react';
 
 class SearchBar extends React.Component {
-  state = { search: "" };
+  state = { term: '' };
 
-  handleSubmit = (event) => {
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
     event.preventDefault();
 
-    this.props.onSearch(this.state.search);
+    this.props.onFormSubmit(this.state.term);
   };
 
   render() {
     return (
-      <div className="ui segemnt">
-        <form className="ui form" onSubmit={this.handleSubmit}>
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>Search Video</label>
+            <label>Video Search</label>
             <input
               type="text"
-              value={this.state.search}
-              onChange={(val) => this.setState({ search: val.target.value })}
+              value={this.state.term}
+              onChange={this.onInputChange}
             />
           </div>
         </form>
