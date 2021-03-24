f7b841406d4ee612994954ca349bf517a9a08413
arvind l <arvindarvind2210@gmail.com>
2021-03-24 09:15:09 +0530

    Initial commit

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+
+  //native event listeners are executed first than the html event listeners
+  //if mouse clicked on the drop down don't execute this else execute the following code
+  useEffect(() => {
+    document.body.addEventListener("click", (event) => {
+      console.log(event.target);
+      setOpen(false);
+    });
+  }, []);
+
   const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
     return (
       <div
         key={option.value}
@@ -17,10 +32,15 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={` menu ${open ? "visible transition" : ""} `}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
