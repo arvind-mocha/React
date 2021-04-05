24c7751f88166a0bc464804a185c72cf5a005e15
arvind l <arvindarvind2210@gmail.com>
2021-04-05 09:27:13 +0530

    Initial commit

--- songs/src/index.js
+++ songs/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import App from "./components/App";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+// Provider will be connected by the connect function for data
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
