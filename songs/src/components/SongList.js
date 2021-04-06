1d5f1ef962a95b6c87e47d0c42e8a744ad33ef01
arvind l <arvindarvind2210@gmail.com>
2021-04-06 20:14:49 +0530

    Initial commit

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
-  render() {
-    console.log(this.props);
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
 
-    return <div>SongList</div>;
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -19,5 +36,6 @@ const mapStateToProps = (state) => {
 //here we connect provider with a react component
 
 //the parameter for the connect function is a function where we get the state from the provider which is called every time when the state changes
-//the parameter fo the function inside connect is the react component
-export default connect(mapStateToProps)(SongList);
+//the second parameter for the connect function is the props which is passes to the component
+//the parameter for the function inside connect is the react component
+export default connect(mapStateToProps, { selectSong })(SongList);
