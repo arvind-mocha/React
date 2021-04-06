4842bf675a3616661a06a1125195310d03bd0696
arvind l <arvindarvind2210@gmail.com>
2021-04-06 13:34:20 +0530

    Initial commit

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -13,4 +13,11 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 };
 
+//connect is a function
+//we are calling function inside connect
+
+//here we connect provider with a react component
+
+//the parameter for the connect function is a function where we get the state from the provider which is called every time when the state changes
+//the parameter fo the function inside connect is the react component
 export default connect(mapStateToProps)(SongList);
