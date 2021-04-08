f528c1181457093828527b4b181b369ef86c1feb
Arvind-mocha <64320021+arvind-mocha@users.noreply.github.com>
2021-04-08 20:38:43 +0530

    Initial commit

--- songs/src/index.js
+++ songs/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import App from './components/App';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./components/App";
+import reducers from "./reducers";
 
+//provider provides data to the connect
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
-  document.querySelector('#root')
+  document.querySelector("#root")
 );
--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,16 +1,17 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
 const songsReducer = () => {
   return [
-    { title: 'No Scrubs', duration: '4:05' },
-    { title: 'Macarena', duration: '2:30' },
-    { title: 'All Star', duration: '3:15' },
-    { title: 'I Want it That Way', duration: '1:45' }
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "All Star", duration: "3:15" },
+    { title: "I Want it That Way", duration: "1:45" },
   ];
 };
 
+//selectedSongReducer is called when state changes
 const selectedSongReducer = (selectedSong = null, action) => {
-  if (action.type === 'SONG_SELECTED') {
+  if (action.type === "SONG_SELECTED") {
     return action.payload;
   }
 
@@ -19,5 +20,5 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
 export default combineReducers({
   songs: songsReducer,
-  selectedSong: selectedSongReducer
+  selectedSong: selectedSongReducer,
 });
