fa2a6060d80c6b9186e2850fec9fd7b9e42d97a5
Arvind-mocha <64320021+arvind-mocha@users.noreply.github.com>
2021-04-12 20:46:27 +0530

    Initial commit

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,15 @@
-export const fetchPosts = () => {
-  return { type: "FETCH_POST" };
+import jsonPlaceholder from "../apis/JsonPlaceHolder";
+
+// we can't use async await with redux. when it is compiled to es15 syntax it will cause error
+//so can can use a middle ware called redux-thunk instead
+//using redux thunk only we can return function. So we can use asyn await to the returned function which won't cause error
+//using redux-thunk as a advantage if we return a function like below, we can get the dispatch and getstate function by using redux-thunk
+//returning a function is totaly an option we can also return objects like we always do
+
+export const fetchPosts = () => async (dispatch, getState) => {
+  const response = await jsonPlaceholder.get("/post");
+
+  //since we are using dispatch itself we don't need to return anything we can directly perform our action
+
+  dispatch({ type: "FETCH_POST", payload: response });
 };
--- blog/src/index.js
+++ blog/src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import Reducer from "./reducers";
+import thunk from "redux-thunk";
 
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+
+//Integrating our middle ware redux-thunk
+//It as it's purpose and some advantages to
+const store = createStore(Reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={createStore(Reducer)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
