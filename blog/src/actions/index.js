8dbca0472edaa2566c4dee0e7cfb2ce6720290d3
Arvind-mocha <64320021+arvind-mocha@users.noreply.github.com>
2021-05-04 20:25:37 +0530

    Initial message

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,4 @@
-import jsonPlaceholder from "../apis/JsonPlaceHolder";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 // we can't use async await with redux. when it is compiled to es15 syntax it will cause error
 //so can can use a middle ware called redux-thunk instead
@@ -6,10 +6,16 @@ import jsonPlaceholder from "../apis/JsonPlaceHolder";
 //using redux-thunk as a advantage if we return a function like below, we can get the dispatch and getstate function by using redux-thunk
 //returning a function is totaly an option we can also return objects like we always do
 
-export const fetchPosts = () => async (dispatch, getState) => {
-  const response = await jsonPlaceholder.get("/post");
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
 
   //since we are using dispatch itself we don't need to return anything we can directly perform our action
 
-  dispatch({ type: "FETCH_POST", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/users/${id}");
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
-export default combineReducers({ replaceMe: () => "hi" });
+export default combineReducers({ posts: postsReducer });
