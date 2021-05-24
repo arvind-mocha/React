be76bb0bc06b9dfc86b8655b11531c6a180413ea
Arvind-mocha <64320021+arvind-mocha@users.noreply.github.com>
2021-05-24 19:39:20 +0530

    Initial commit

--- songs/src/components/songDetail.js
+++ songs/src/components/songDetail.js
@@ -4,20 +4,20 @@ import { connect } from "react-redux";
 //the value of this parameter song comes from the mapStateToProps function
 const SongDetail = (props) => {
   console.log(props);
-  // if (!song) {
-  //   return <div>Select a song</div>;
-  // }
-  // return (
-  //   <div>
-  //     <h3>Details for:</h3>
-  //     <p>
-  //       Title: {props.song.title}
-  //       <br />
-  //       <br />
-  //       Duration: {props.song.duration}
-  //     </p>
-  //   </div>
-  // );
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {props.song.title}
+        <br />
+        <br />
+        Duration: {props.song.duration}
+      </p>
+    </div>
+  );
 
   return "hi";
 };
